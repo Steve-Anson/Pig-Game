d37120ffbeaf38779e0399246c0f9e0f9a344b4a
Steve-Anson <steveanson12@gmail.com>
2023-10-05 22:12:30 +0530

    Add Current Score Updation Functionality

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +15,8 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -23,4 +28,11 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `images/dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
 });
